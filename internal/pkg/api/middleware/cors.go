64f6c196870f6f1a6a80a235a65f8e74aef33fed
Tung Teng <thetungctn@gmail.com>
2026-04-15 16:49:15 +0700

    Allow CORS for TUS uploads

--- internal/pkg/api/middleware/cors.go
+++ internal/pkg/api/middleware/cors.go
@@ -9,13 +9,36 @@ import (
 
 func NewCORSMiddleware() gin.HandlerFunc {
 	corsConfig := cors.DefaultConfig()
-	corsConfig.AddAllowMethods(http.MethodOptions)
+	corsConfig.AddAllowMethods(
+		http.MethodOptions,
+		http.MethodHead,
+		http.MethodPatch,
+		http.MethodPost,
+	)
 	corsConfig.AllowAllOrigins = true
 
 	corsConfig.AddAllowHeaders("Authorization")
 	corsConfig.AddAllowHeaders("X-Request-ID")
 	corsConfig.AddAllowHeaders("X-Client-ID")
 	corsConfig.AddAllowHeaders("Accept-Version")
+	corsConfig.AddAllowHeaders("Tus-Resumable")
+	corsConfig.AddAllowHeaders("Upload-Length")
+	corsConfig.AddAllowHeaders("Upload-Metadata")
+	corsConfig.AddAllowHeaders("Upload-Offset")
+	corsConfig.AddAllowHeaders("Tus-Version")
+	corsConfig.AddAllowHeaders("Tus-Extension")
+	corsConfig.AddAllowHeaders("Tus-Max-Size")
+	corsConfig.AddAllowHeaders("Content-Type")
+
+	corsConfig.AddExposeHeaders("Location")
+	corsConfig.AddExposeHeaders("Tus-Resumable")
+	corsConfig.AddExposeHeaders("Upload-Offset")
+	corsConfig.AddExposeHeaders("Upload-Length")
+	corsConfig.AddExposeHeaders("Upload-Metadata")
+	corsConfig.AddExposeHeaders("Tus-Version")
+	corsConfig.AddExposeHeaders("Tus-Extension")
+	corsConfig.AddExposeHeaders("Tus-Max-Size")
+	corsConfig.AddExposeHeaders("X-Job-IDs")
 
 	return cors.New(corsConfig)
 }
